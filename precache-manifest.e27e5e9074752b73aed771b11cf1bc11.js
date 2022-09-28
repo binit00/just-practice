704a5bb5df36ee76d933a4c3126a2893c3639707
Binit kumar <binitkrr789@gmail.com>
2022-09-28 16:02:25 +0530

    Updates

---
+++ precache-manifest.e27e5e9074752b73aed771b11cf1bc11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13db75b969133b6472e3",
+    "url": "/static/css/main.f51402cd.chunk.css"
+  },
+  {
+    "revision": "13db75b969133b6472e3",
+    "url": "/static/js/main.13db75b9.chunk.js"
+  },
+  {
+    "revision": "cf968a8b3c11f809e8bf",
+    "url": "/static/js/1.cf968a8b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ba4ed60de5c329dacd9976e2ebbf510",
+    "url": "/index.html"
+  }
+];
